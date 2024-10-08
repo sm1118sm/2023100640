a3bd41b67702c43557ae1cb82c0dde625d59ac99
sm1118sm <sm1118sm@gmail.com>
2024-10-08 21:52:03 +0900

    Update 팰린드롬인지 확인하기.c

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.c
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-
-int main(void) {
-
-	char word[101];
-
-	scanf("%s", &word);
-
-	int first = 0;
-	int last = strlen(word);
-	int mid = (first + last) / 2;
-
-	int result = 1;
-
-
-	for (int i = 0; i < mid; i++)
-	{
-		if (word[i] != word[last - i - 1]) {
-			result = 0;
-			break;
-		}
-	}
-
-	printf("%d", result);
-
+int main(){
+    char a[10000];
+    
+    scanf("%s", &a[0]);
+    
+    int len = strlen(a);
+    int count = 0;
+    
+    for (int i = 0; i < len / 2; i++){
+        if (a[i] == a[len - i - 1]){
+            count++;
+        }
+    }
+    
+    if (count == (len / 2)){
+        printf("%d", 1);
+    }
+    
+    else{
+        printf("%d", 0);
+    }
+    
     return 0;
 }
