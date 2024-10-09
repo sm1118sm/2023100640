14a25c60618a0faeaa097fc7bf4d71dc1170fe25
sm1118sm <sm1118sm@gmail.com>
2024-10-09 23:16:26 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    
+    scanf("%d", &n);
+    
+    
+    for (int i = n; i > 0 ; i--) {
+        printf("%d\n", i);
+    }
+    
+
+    return 0;
+}
